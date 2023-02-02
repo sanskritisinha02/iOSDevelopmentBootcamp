ebf8c46ac7463658573deab0b34fdc68b2fbd8a8
Sanskriti Sinha <sanskritisinha.02@gmail.com>
2023-02-02 15:44:47 +0530

    Add files via upload

---
+++ IamPoor/IamPoor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IamPoor
+//
+//  Created by Sanskriti Sinha on 02/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
